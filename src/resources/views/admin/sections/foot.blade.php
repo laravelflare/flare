23109a0b8d3012ae5aae793d3affe0e8c8b44ccb
Aden Fraser <aden@jacobbailey.com>
2015-09-23 17:31:08 +0100

    Bootstrap JS link corrected in footer

--- src/resources/views/admin/sections/foot.blade.php
+++ src/resources/views/admin/sections/foot.blade.php
@@ -1,5 +1,5 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="{{ asset ('vendor/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset ('vendor/flare/js/bootstrap.min.js') }}" type="text/javascript"></script>
         <script src="{{ asset ('vendor/flare/dist/js/app.min.js') }}" type="text/javascript"></script>
 
         @yield('enqueued-js')
