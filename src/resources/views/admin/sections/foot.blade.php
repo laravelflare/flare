7925df39bd3fcea0d4ff6e5554a06af4e81cb065
Aden Fraser <aden@jacobbailey.com>
2015-09-28 13:44:22 +0100

    Adds AdminLte plugins to dist and adds Morris charts to header and footer dependencies

--- src/resources/views/admin/sections/foot.blade.php
+++ src/resources/views/admin/sections/foot.blade.php
@@ -2,6 +2,9 @@
         <script src="{{ asset ('vendor/flare/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
         <script src="{{ asset ('vendor/flare/dist/js/app.min.js') }}" type="text/javascript"></script>
 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+        <script src="{{ asset ('vendor/flare/dist/plugins/morris/morris.min.js') }}"></script>
+
         @yield('enqueued-js')
     </body>
 </html>
--- src/resources/views/admin/sections/head.blade.php
+++ src/resources/views/admin/sections/head.blade.php
@@ -7,8 +7,10 @@
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+
         <link href="{{ asset('vendor/flare/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
         <link href="{{ asset('vendor/flare/dist/css/skins/skin-'.Flare::config('admin_theme').'.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('vendor/flare/dist/plugins/morris/morris.css') }}" rel="stylesheet" type="text/css" />
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
