ae912fdd8ff8755b7298bc13909225537e55da40
Aden Fraser <aden@jacobbailey.com>
2015-10-04 00:28:27 +0100

    Bring back search form

--- src/resources/views/admin/sections/sidebar.blade.php
+++ src/resources/views/admin/sections/sidebar.blade.php
@@ -10,6 +10,15 @@
             </div>
         </div>
 
+        <form class="sidebar-form" method="get" action="#">
+            <div class="input-group">
+                <input type="text" placeholder="Search..." class="form-control" name="q">
+                <span class="input-group-btn">
+                    <button class="btn btn-flat" id="search-btn" name="search" type="submit"><i class="fa fa-search"></i></button>
+                </span>
+            </div>
+        </form>
+
         <ul class="sidebar-menu">
             <li class="{{ Request::is('admin') ? 'active' : '' }}">
                 <a href="{{ Flare::adminUrl() }}">
