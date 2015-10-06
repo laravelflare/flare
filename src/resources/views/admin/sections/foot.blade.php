241d214a35f88957150e20840c33177c3ad90708
Aden Fraser <aden@jacobbailey.com>
2015-10-06 01:31:13 +0100

    Amend head.blade.php and foot.blade.php to load in Bower published assets

--- src/resources/views/admin/sections/foot.blade.php
+++ src/resources/views/admin/sections/foot.blade.php
@@ -1,9 +1,10 @@
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="{{ asset ('vendor/flare/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset ('vendor/flare/dist/js/app.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('vendor/flare/plugins/jQuery/jQuery-2.1.4.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('vendor/flare/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('vendor/flare/js/app.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('vendor/flare/plugins/morris/morris.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('vendor/flare/plugins/select2/select2.full.min.js') }}" type="text/javascript"></script>
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-        <script src="{{ asset ('vendor/flare/dist/plugins/morris/morris.min.js') }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js" type="text/javascript"></script>
 
         @yield('enqueued-js')
     </body>
