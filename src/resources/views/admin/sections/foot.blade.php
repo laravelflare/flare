20e20700940bedee0e70fc8ba02cc17e6f53dffa
Aden Fraser <aden@jacobbailey.com>
2015-10-13 17:32:23 +0100

    Date Mask Field

--- src/resources/views/admin/sections/foot.blade.php
+++ src/resources/views/admin/sections/foot.blade.php
@@ -3,7 +3,9 @@
         <script src="{{ asset('vendor/flare/js/app.min.js') }}" type="text/javascript"></script>
         <script src="{{ asset('vendor/flare/plugins/morris/morris.min.js') }}" type="text/javascript"></script>
         <script src="{{ asset('vendor/flare/plugins/select2/select2.full.min.js') }}" type="text/javascript"></script>
-
+        <script src="{{ asset('vendor/flare/plugins/input-mask/jquery.inputmask.js') }}"></script>
+        <script src="{{ asset('vendor/flare/plugins/input-mask/jquery.inputmask.date.extensions.js') }}"></script>
+        <script src="{{ asset('vendor/flare/plugins/input-mask/jquery.inputmask.extensions.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js" type="text/javascript"></script>
 
         @yield('enqueued-js')
