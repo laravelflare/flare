1cf2bbb3194cc8ff7d3dd2785ac69ac46c30f381
Aden Fraser <aden@jacobbailey.com>
2015-10-15 00:27:01 +0100

    Events for Writeable Actions... Coming soon!

---
+++ src/Flare/Events/EditEvent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LaravelFlare\Flare\Events;
+
+use Illuminate\Queue\SerializesModels;
+
+class EditEvent extends Event
+{
+    use SerializesModels;
+
+    /**
+     * Post.
+     * 
+     * @var Post
+     */
+    public $post;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct()
+    {
+
+    }
+}
---
+++ src/Flare/Events/SaveEvent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LaravelFlare\Flare\Events;
+
+use Illuminate\Queue\SerializesModels;
+
+class SaveEvent extends Event
+{
+    use SerializesModels;
+
+    /**
+     * Post.
+     * 
+     * @var Post
+     */
+    public $post;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct()
+    {
+
+    }
+}
