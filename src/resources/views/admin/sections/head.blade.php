0734651d37fec76cc6b7fb5205355646a563339d
Aden Fraser <aden@jacobbailey.com>
2015-10-17 20:40:31 +0100

    Use Safe Admin Title as the Default <title> tag if a page_title cannot be yielded

--- src/resources/views/admin/sections/head.blade.php
+++ src/resources/views/admin/sections/head.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>@yield('page_title', 'Flare')</title>
+        <title>@yield('page_title', Flare::safeAdminTitle())</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
