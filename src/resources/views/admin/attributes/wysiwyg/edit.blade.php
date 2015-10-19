930c24c997a5c737cd7c86e5aab7be7dfaa91dc9
Aden Fraser <aden@jacobbailey.com>
2015-10-19 13:18:35 +0100

    Added Wysiwyg Functionality

---
+++ src/resources/views/admin/attributes/wysiwyg/edit.blade.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="col-sm-6">
+        <div class="form-group @if ($errors->has($attribute)) has-error @endif">
+            <label class="control-label" for="{{ $attribute }}">
+                {{ $attributeTitle }} @if (isset($field['required'])) * @endif
+            </label>
+            <textarea id="{{ $attribute }}"
+                        class="form-control {{ $field['class'] or null }}"
+                        name="{{ $attribute }}"
+                        @if (isset($field['required'])) required="required" @endif>{{ old($attribute, $modelManager->getAttribute($attribute, $model) ) }}</textarea>
+            @if ($errors->has($attribute))
+                <span class="help-block">
+                    {{ $errors->first($attribute) }}
+                </span>
+            @endif
+        </div>
+    </div>
+</div>
+
+@section('enqueued-js')
+    <script>
+    $(function () {
+        $("#{{ $attribute }}").wysihtml5();
+    });
+    </script>
+@append 
