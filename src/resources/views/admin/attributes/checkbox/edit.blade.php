a1a65a135db06395a5eda89f1bee261f56edd1d2
Aden Fraser <aden@jacobbailey.com>
2015-10-19 12:18:20 +0100

    Use getAttributeFromArray for all multi-option fields

--- src/resources/views/admin/attributes/checkbox/edit.blade.php
+++ src/resources/views/admin/attributes/checkbox/edit.blade.php
@@ -15,9 +15,9 @@
                                     name="{{ $attribute }}{{ (count($field['options']) > 1 ? '[]' : '') }}"
                                     @if (isset($field['required'])) required="required" @endif
                                     @if (
-                                            (is_string($modelManager->getAttribute($attribute, $model)) && $modelManager->getAttribute($attribute, $model) == $value)
+                                            (is_string($modelManager->getAttributeFromArray($attribute, $model)) && $modelManager->getAttributeFromArray($attribute, $model) == $value)
                                         ||
-                                            (is_array($modelManager->getAttribute($attribute, $model)) && array_key_exists($value, $modelManager->getAttribute($attribute, $model)))
+                                            (is_array($modelManager->getAttributeFromArray($attribute, $model)) && array_key_exists($value, $modelManager->getAttributeFromArray($attribute, $model)))
                                         )
                                         checked="checked" @endif
                                     >
