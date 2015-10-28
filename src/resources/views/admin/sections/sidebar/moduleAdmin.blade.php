7191256fdeb9f996b379d57f433c06d969af3379
Aden Fraser <aden@jacobbailey.com>
2015-10-28 20:05:34 +0000

    Remove Legacy Code

--- src/resources/views/admin/sections/sidebar/moduleAdmin.blade.php
+++
@@ -1,8 +0,0 @@
-<li class="treeview {{ Request::is( $adminItem::relativeUrl() . '*' ) ? 'active' : '' }}">
-    <a href="{{ $adminItem::url() }}">
-        @if ($adminItem::getIcon())
-        <i class="fa fa-{{ $adminItem::getIcon() }}"></i>
-        @endif
-        <span>{{ $adminItem::pluralTitle() }}</span>
-    </a>
-</li>
