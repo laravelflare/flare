a1fb8027a76a59e8d464a53921b2593015b31a28
Aden Fraser <aden@jacobbailey.com>
2015-11-04 23:00:49 +0000

    Add tooltips to all labels as an option, and also to all required fields

--- src/resources/views/admin/attributes/wysiwyg/add.blade.php
+++ src/resources/views/admin/attributes/wysiwyg/add.blade.php
@@ -2,7 +2,13 @@
     <div class="col-sm-6">
         <div class="form-group @if ($errors->has($attribute)) has-error @endif">
             <label class="control-label" for="{{ $attribute }}">
-                {{ $attributeTitle }} @if (isset($field['required'])) * @endif
+                {{ $attributeTitle }}
+                @if (isset($field['required'])) 
+                <span title="" data-placement="right" data-toggle="tooltip" data-original-title="This field is required">*</span>
+                @endif
+                @if(isset($field['tooltip']))
+                <span title="" data-placement="right" data-toggle="tooltip" class="badge bg-black" data-original-title="{{ $field['tooltip'] }}">?</span>
+                @endif
             </label>
             <textarea id="{{ $attribute }}"
                         class="form-control {{ $field['class'] or null }}"
