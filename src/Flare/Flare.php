45b250085552404de17f1f5722a66207ca4ce89c
Aden Fraser <aden@jacobbailey.com>
2015-11-04 23:50:54 +0000

    Fix Attributes Available call from resolver

--- src/Flare/Flare.php
+++ src/Flare/Flare.php
@@ -190,7 +190,7 @@ class Flare
      */
     protected function resolveAttributeClass($type)
     {
-        $fullClassname = array_search(title_case($type), $this->availableAttributes);
+        $fullClassname = array_search(title_case($type), $this->availableAttributes());
 
         if (!$fullClassname || !class_exists($fullClassname)) {
             return false;
