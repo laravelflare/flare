1ae98d82aa158fe01461199497ff85e452205b57
Aden Fraser <aden@jacobbailey.com>
2015-11-06 20:26:13 +0000

    Adjust footer indents

--- src/resources/views/admin/sections/footer.blade.php
+++ src/resources/views/admin/sections/footer.blade.php
@@ -1,13 +1,13 @@
-        <footer class="main-footer">
-            <span class="pull-right hidden-xs" style="padding-left: 10px; padding-top: 1px">
-                Version: <b>{{ Flare::version() }}</b> 
-            </span>
-            <a href="https://github.com/laravelflare/flare" target="_blank">
-                <span class="btn btn-social-icon btn-github btn-xs">
-                    <i class="fa fa-github"></i>
-                </span>
-                <span class="text-red" style="padding-left: 10px; top: 1px; position: relative;">
-                    Laravel <strong>Flare</strong>
-                </span>
-            </a>
-        </footer>
+<footer class="main-footer">
+    <span class="pull-right hidden-xs" style="padding-left: 10px; padding-top: 1px">
+        Version: <b>{{ Flare::version() }}</b> 
+    </span>
+    <a href="https://github.com/laravelflare/flare" target="_blank">
+        <span class="btn btn-social-icon btn-github btn-xs">
+            <i class="fa fa-github"></i>
+        </span>
+        <span class="text-red" style="padding-left: 10px; top: 1px; position: relative;">
+            Laravel <strong>Flare</strong>
+        </span>
+    </a>
+</footer>
