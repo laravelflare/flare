adddc00d872b0578df096f0510b01c23b13a64c7
Aden Fraser <aden@jacobbailey.com>
2016-01-30 22:07:18 +0000

    File and Image Attributes

---
+++ src/resources/views/admin/attributes/file/add.blade.php
@@ -0,0 +1,36 @@
+<div class="row">
+    <div class="col-sm-6">
+        <div class="form-group @if ($errors->has($attribute)) has-error @endif">
+            <label class="control-label" for="{{ $attribute }}">
+                {{ $attributeTitle }}
+                @if (isset($field['required'])) 
+                <span title="" data-placement="right" data-toggle="tooltip" data-original-title="This field is required">*</span>
+                @endif
+                @if(isset($field['tooltip']))
+                <span title="" data-placement="right" data-toggle="tooltip" class="badge bg-black" data-original-title="{{ $field['tooltip'] }}">?</span>
+                @endif
+            </label>
+            <input id="{{ $attribute }}"
+                    class="form-control {{ $field['class'] or null }}"
+                    type="file"
+                    name="{{ $attribute }}"
+                    @if (isset($field['accept'])) accept="{{ $field['accept'] }}" @endif
+                    @if (isset($field['maxlength'])) maxlength="{{ $field['maxlength'] }}" @endif
+                    @if (isset($field['disabled'])) disabled="disabled" @endif
+                    @if (isset($field['autofocus'])) autofocus="autofocus" @endif
+                    @if (isset($field['required'])) required="required" @endif
+                    @if (isset($field['multiple'])) multiple="{{ $field['multiple'] }}" @endif
+                >
+                    
+            @if(isset($field['help']))
+                <p class="help-block">{!! $field['help'] !!}</p>
+            @endif
+
+            @if ($errors->has($attribute))
+                <p class="help-block">
+                    <strong>{{ $errors->first($attribute) }}</strong>
+                </p>
+            @endif
+        </div>
+    </div>
+</div>
---
+++ src/resources/views/admin/attributes/file/view.blade.php
@@ -0,0 +1,8 @@
+<dl class="dl-horizontal">
+    <dt>
+        {{ $attributeTitle }}
+    </dt>
+    <dd>
+        {{ $modelManager->getAttribute($attribute, $model)  }}
+    </dd>
+</dl>
