cc3baf93b3ff1fd3e293e187ad6a8a60608a033f
Aden Fraser <aden@jacobbailey.com>
2016-02-16 22:30:42 +0000

    Add FlareModuleProvider

---
+++ src/Flare/FlareModuleProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace LaravelFlare\Flare;
+
+use Illuminate\Support\ServiceProvider;
+
+class FlareModuleProvider extends ServiceProvider
+{
+    /**
+     * Flare
+     * 
+     * @var \LaravelFlare\Flare\Flare
+     */
+    protected $flare;
+
+    /**
+     * Create a new service provider instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @return void
+     */
+    public function __construct($app)
+    {
+        parent::__construct($app);
+        
+        $this->flare = $this->app->make('flare');
+    }
+    
+    /**
+     * Perform post-registration booting of services.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register any package services.
+     */
+    public function register()
+    {
+        
+    }
+}
