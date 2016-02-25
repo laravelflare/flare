a9dfdd1849f94cc183dae3aec44a1f28498786e2
Aden Fraser <aden@jacobbailey.com>
2016-02-25 17:29:09 +0000

    Ability to override the ModelItemList and ModelItemListFooter

--- src/resources/views/admin/modeladmin/includes/modelitem-list-footer.blade.php
+++ src/resources/views/admin/modeladmin/includes/modelitem-list-footer.blade.php
@@ -1,20 +1,24 @@
-<div class="box-footer clearfix">
-    @if ($modelAdmin->hasCreating())
-        <div class="pull-left">
-            <a href="{{ $modelAdmin->currentUrl('create') }}" class="btn btn-success">
-                <i class="fa fa-{{ $modelAdmin->getIcon() }}"></i>
-                Add {{ $modelAdmin->getTitle() }}
-            </a>
-        </div>
-    @endif
+@if (method_exists($modelAdmin, 'modelItemListFooter'))
+    {{ $modelAdmin->modelItemListFooter($modelItems) }}
+@else
+    <div class="box-footer clearfix">
+        @if ($modelAdmin->hasCreating())
+            <div class="pull-left">
+                <a href="{{ $modelAdmin->currentUrl('create') }}" class="btn btn-success">
+                    <i class="fa fa-{{ $modelAdmin->getIcon() }}"></i>
+                    Add {{ $modelAdmin->getTitle() }}
+                </a>
+            </div>
+        @endif
 
-    @if ($modelAdmin->getPerPage())
-        <div class="pull-right" style="margin-top: -20px; margin-bottom: -20px;">
-            {!! $modelItems->appends([
-                                    'sort' => $modelAdmin->sortBy() == 'asc' ? 'asc' : null,
-                                    'order' => $modelAdmin->orderBy(),
-                                    'filter' => \Request::get('filter'),
-                                ])->render() !!}
-        </div>
-    @endif
-</div>
+        @if ($modelAdmin->getPerPage())
+            <div class="pull-right" style="margin-top: -20px; margin-bottom: -20px;">
+                {!! $modelItems->appends([
+                                        'sort' => $modelAdmin->sortBy() == 'asc' ? 'asc' : null,
+                                        'order' => $modelAdmin->orderBy(),
+                                        'filter' => \Request::get('filter'),
+                                    ])->render() !!}
+            </div>
+        @endif
+    </div>
+@endif
