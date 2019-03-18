9b40388e0a28a92e8a9ba5196c28603e4a230dde
Aden Fraser <aden.fraser@bc-legal.co.uk>
2019-03-18 19:23:38 +0000

    Corrects a bug with filter dropdowns

--- src/resources/views/admin/modeladmin/includes/header/filter.blade.php
+++ src/resources/views/admin/modeladmin/includes/header/filter.blade.php
@@ -18,31 +18,33 @@
 
 @if ($modelAdmin->filters())
     <div class="btn-group">
-        <a href="{{ $modelAdmin->currentUrl($type.'?filter='.\Request::get('filter')) }}" class="btn btn-default btn-flat">
-            Filter:
-            @if ($filter = \Request::get('filter'))
-                {{ $modelAdmin->safeFilters()[$filter] }}
-            @else
-                None
-            @endif
-        </a>
-        <button data-toggle="dropdown" class="btn btn-default btn-flat dropdown-toggle" type="button">
-            <span class="caret"></span>
-            <span class="sr-only">Toggle Dropdown</span>
-        </button>
-        <ul role="menu" class="dropdown-menu">
-            <li>
-                <a href="{{ $modelAdmin->currentUrl($type) }}">
+        <div class="btn-group">
+            <a href="{{ $modelAdmin->currentUrl($type.'?filter='.\Request::get('filter')) }}" class="btn btn-default btn-flat">
+                Filter:
+                @if ($filter = \Request::get('filter'))
+                    {{ $modelAdmin->safeFilters()[$filter] }}
+                @else
                     None
-                </a>
-            </li>
-            @foreach ($modelAdmin->safeFilters() as $filterAction => $filter)
+                @endif
+            </a>
+            <button data-toggle="dropdown" class="btn btn-default btn-flat dropdown-toggle" type="button">
+                <span class="caret"></span>
+                <span class="sr-only">Toggle Dropdown</span>
+            </button>
+            <ul role="menu" class="dropdown-menu">
                 <li>
-                    <a href="{{ $modelAdmin->currentUrl($type.'?filter='.$filterAction) }}">
-                       {{ $filter }}
+                    <a href="{{ $modelAdmin->currentUrl($type) }}">
+                        None
                     </a>
                 </li>
-            @endforeach
-        </ul>
+                @foreach ($modelAdmin->safeFilters() as $filterAction => $filter)
+                    <li>
+                        <a href="{{ $modelAdmin->currentUrl($type.'?filter='.$filterAction) }}">
+                           {{ $filter }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 @endif
